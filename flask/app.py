25715c36cbb34a51f2708789850310571c06a5a6
Christopher Cahill <cthomascahill@gmail.com>
2019-10-02 17:45:47 -0700

    basic flask hello world

--- flask/app.py
+++ flask/app.py
@@ -1,28 +1,31 @@
+import os
 from flask import Flask
-from flask import jsonify
-from flask import request
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
-
-cors = CORS(app, resources={r"*": {"origins": "*"}})
-# cors = CORS(app)
-
-@app.route('/links', methods=['POST'])
-@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
-
-def index():
-    data = request.json
-    print(data)
-    return jsonify(Links=scrape(data))
-    # return jsonify(data)
-    # print(data['url'])
-    # return data
-
-@app.route('/click/<variable>', methods=['GET'])
-def index2(variable):
-    return jsonify(Links=scrapeClick(variable))
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
