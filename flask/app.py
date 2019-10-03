b6c3cce73bad989cee4b0a56c9d907c639bb7ea4
Christopher Cahill <cthomascahill@gmail.com>
2019-10-02 17:26:06 -0700

    add flask directory

---
+++ flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+# cors = CORS(app)
+
+@app.route('/links', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+
+def index():
+    data = request.json
+    print(data)
+    return jsonify(Links=scrape(data))
+    # return jsonify(data)
+    # print(data['url'])
+    # return data
+
+@app.route('/click/<variable>', methods=['GET'])
+def index2(variable):
+    return jsonify(Links=scrapeClick(variable))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
